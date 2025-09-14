b18a45a71ed2239434340e2e540f1c0d6079efbe
Prathamesh Chougale <prathamesh17170@gmail.com>
2025-09-14 12:16:07 +0530

    feat: integrate Toaster component for enhanced user notifications
    - Added the Toaster component to the RootLayout for improved user feedback and notifications throughout the application.
    - Ensured seamless integration within the existing layout structure, maintaining consistent styling and functionality.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AvaxWalletProvider from "@/components/avax-wallet/provider";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,6 +38,7 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             {children}
+            <Toaster />
           </ThemeProvider>
         </AvaxWalletProvider>
       </body>
